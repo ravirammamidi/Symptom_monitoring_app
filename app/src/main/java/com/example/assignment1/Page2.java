d60b6762070bb953b389fc4d17da55f5ed6e549c
ravirammamidi <tatsuya.s.mknr@gmail.com>
2021-09-27 19:47:16 -0700

    First Stage all functionalities working - Heart (not accurate) - Respiration (somewhat accurate) - Symptoms (working fine)

--- app/src/main/java/com/example/assignment1/Page2.java
+++ app/src/main/java/com/example/assignment1/Page2.java
@@ -24,48 +24,40 @@ import java.util.Map;
 
 public class Page2 extends AppCompatActivity {
 
-    //Global Variables
-    private static String[] symptoms_items = new String[]{"Nausea", "Headache", "Sore Throat","Diarrhea","Fever","Muscle Ache","Loss of smell or taste","Cough","Shortness of breath","Feeling Tired"};
-    private static ArrayList<String> selected_symptoms= new ArrayList<String>();
-    private static ArrayList<Float> selected_rating= new ArrayList<Float>();
-    private static Map<String,Float> combined_list;
-    static {
-        combined_list = new HashMap<>();
-        combined_list.put("Nausea", Float.intBitsToFloat(0));
-        combined_list.put("Headache", Float.intBitsToFloat(0) );
-        combined_list.put("Sore Throat", Float.intBitsToFloat(0) );
-        combined_list.put("Diarrhea", Float.intBitsToFloat(0) );
-        combined_list.put("Fever", Float.intBitsToFloat(0) );
-        combined_list.put("Muscle Ache", Float.intBitsToFloat(0) );
-        combined_list.put("Loss of smell or taste", Float.intBitsToFloat(0) );
-        combined_list.put("Cough", Float.intBitsToFloat(0) );
-        combined_list.put("Shortness of breath", Float.intBitsToFloat(0) );
-        combined_list.put("Feeling Tired", Float.intBitsToFloat(0) );
-    }
 
+    //Global Variables
+    public String[] symptoms_items = new String[]{"Nausea", "Headache", "SoreThroat","Diarrhea","Fever","MuscleAche","LossOfSmellOrTaste","Cough","ShortnessOfBreath","FeelingTired"};
+    public Integer symtomToInt(String symptom){
+        return  Arrays.asList(symptoms_items).indexOf(symptom);
+    }
+    public float[] res_v=new float[symptoms_items.length];
     // using above item list initialize an array
 
 
 
-//    public class SpinnerActivity extends Activity implements AdapterView.OnItemSelectedListener {
-//        @Override
-//        public void onItemSelected(AdapterView<?> parent, View view,
-//                                   int position, long id) {
-//            Log.i("ITEM", (String) parent.getItemAtPosition(position));
-//            selected_symptoms.add((String) parent.getItemAtPosition(position));
-//        }
-//        @Override
-//        public void onNothingSelected(AdapterView<?> parent) {
-//            // TODO Auto-generated method stub
-//        }
-//    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
 
+        ArrayList<String> selected_symptoms= new ArrayList<String>();
+        ArrayList<Float> selected_rating= new ArrayList<Float>();
+        Map<String,Float> combined_list;
+        combined_list = new HashMap<>();
+        combined_list.put("Nausea", Float.intBitsToFloat(0));
+        combined_list.put("Headache", Float.intBitsToFloat(0) );
+        combined_list.put("SoreThroat", Float.intBitsToFloat(0) );
+        combined_list.put("Diarrhea", Float.intBitsToFloat(0) );
+        combined_list.put("Fever", Float.intBitsToFloat(0) );
+        combined_list.put("MuscleAche", Float.intBitsToFloat(0) );
+        combined_list.put("LossOfSmellOrTaste", Float.intBitsToFloat(0) );
+        combined_list.put("Cough", Float.intBitsToFloat(0) );
+        combined_list.put("ShortnessOfBreath", Float.intBitsToFloat(0) );
+        combined_list.put("FeelingTired", Float.intBitsToFloat(0) );
 
+
+        // SPINNER INITIALIZATION
         Spinner dropdown = findViewById(R.id.spinner);
         //create an adapter to describe how the items are displayed, adapters are used in several places in android.
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, symptoms_items);
@@ -93,10 +85,13 @@ public class Page2 extends AppCompatActivity {
                     }
                 });
                 RatingBar ratingBar=findViewById(R.id.ratingBar);
-                float getrating = ratingBar.getRating();
-                selected_rating.add(getrating);
-                Log.i("SYMP_LIST",String.valueOf(getrating) + " ");
-                Toast.makeText(view.getContext(), "Symptom Saved "+selected_symptoms.size()+" "+getrating,Toast.LENGTH_LONG).show();
+                float rating = ratingBar.getRating();
+                selected_rating.add(rating);
+                Log.i("SYMP_LIST",String.valueOf(rating) + " ");
+                Toast.makeText(view.getContext(), "Symptom Saved "+selected_symptoms.size()+" "+rating,Toast.LENGTH_SHORT).show();
+                if(selected_symptoms.size()!=0){ // saving the entry
+                    combined_list.replace(selected_symptoms.get(selected_symptoms.size()-1),rating);
+                }
             }
         });
 
@@ -107,13 +102,22 @@ public class Page2 extends AppCompatActivity {
             public void onClick(View view) {
                 // Code for pushing data into db
 
+                String res="";
 
-
-                Toast.makeText(view.getContext(),"List saved and pushed to DB",Toast.LENGTH_LONG).show();
+                Toast.makeText(view.getContext(),"List saved and ready ",Toast.LENGTH_LONG).show();
+                for (int i=0;i<symptoms_items.length;i++) {
+                    String key = symptoms_items[i];
+                    Float value = combined_list.get(key);
+                    Log.i( "DATALOG","Key Value pairs:" + key +" - "+ value);
+                    res=String.valueOf(value);
+                    res_v[i]=value;
+                    // do stuff
+                }
                 // end for db push
 
                 // For going back to home page
                 Intent int2=new Intent( Page2.this,Page1.class); // send back to main context
+                int2.putExtra("symp_list",res_v);
                 startActivity(int2);
             }
         });
